9ae1ceb81f128edd3d7731767466193891c9027c
unibix <info@unibix.ru>
2026-04-02 19:16:53 +0200

    Update smartajaxformsconfig.php

--- smartajaxformsconfig.php
+++ smartajaxformsconfig.php
@@ -3,17 +3,22 @@
 // имя файла CSV где хранятся лиды, например leadsss.csv, если не указывать, то лиды не будут сохраняться в csv
 define('CSV_FILENAME', '');
 
-// Яндекс Капча
-define('YANDEX_CAPTCHA_SITEKEY', 'ysc1_xMdq3FfSBu2BfbKKt94GDjJfTdhh7bRtystAxzIK3d548870');
+// email для отправки заявок и лидов
+define('EMAIL', ''); //если оставить пустым то не будет отправлять 
 
 // Telegram 
 define('TG_TOKEN', ''); //если оставить пустым то не будет отправлять в телеграм чат лид 
 define('TG_CHAT_ID', ''); //айди чата в котором ваш бот имеет права отправлять сообщения
 
+// Max messenger
+define('MAX_TOKEN', '');  //если оставить пустым то не будет отправлять в макс чат лид 
+define('MAX_CHAT_ID', ''); //айди чата в котором ваш бот имеет права отправлять сообщения
+
 // Метрика для срабатывания целей метрики и передачи дополнительных данных о лиде
 define('YANDEX_METRIKA_ID', 0);
 
-
+// Яндекс Капча
+define('YANDEX_CAPTCHA_SITEKEY', 'ysc1_xMdq3FfSBu2BfbKKt94GDjJfTdhh7bRtystAxzIK3d548870');
 
 
 ?>
